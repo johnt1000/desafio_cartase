91f0b7985fe84325085d92ac4c87925b39f00e06
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-06-12 01:14:32 -0300

    Os projetos agora tem dono

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :amount, presence: true, numericality: true
   validates :image, presence: true
