5bde3a310ba823d5438fef35faa1d93f8f8543b3
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-06-12 00:03:55 -0300

    Adicionado referencia de usuário ao projeto

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
-    mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :amount, presence: true, numericality: true
+  validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
 end
