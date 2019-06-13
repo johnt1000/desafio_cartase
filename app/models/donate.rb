801575c7c0276ba97f02b3f69872dfc9816ee800
Jonatas Rodrigues Alves <jonatas.rodriguess@gmail.com>
2019-06-13 16:09:25 -0300

    Validação de total de arrecadação

--- app/models/donate.rb
+++ app/models/donate.rb
@@ -1,6 +1,16 @@
 class Donate < ApplicationRecord
+  validate :verifica_arrecadacao
+
   belongs_to :user
   belongs_to :project
 
   validates :donate, presence: true, numericality: true
+
+  private
+    def verifica_arrecadacao
+      if self.project.donates.sum(:donate) >= 500
+        errors.add(:donate, 'Arrecadação total é maior que R$ 500.00.')
+      end
+    end
+
 end
