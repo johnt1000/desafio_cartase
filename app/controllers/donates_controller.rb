97f0996e285e7d2e6bfba8ea2c209ff7b4caea0a
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-06-13 15:29:04 -0300

    Ajuste para entidade de doação

--- app/controllers/donates_controller.rb
+++ app/controllers/donates_controller.rb
@@ -1,5 +1,5 @@
 class DonatesController < ApplicationController
-  before_action :set_donate, only: [:show, :edit, :update, :destroy]
+  before_action :set_donate, only: [:show]
   before_action :set_project, only: [:new]
 
   # GET /donates
@@ -18,10 +18,6 @@ class DonatesController < ApplicationController
     @donate = Donate.new
   end
 
-  # GET /donates/1/edit
-  def edit
-  end
-
   # POST /donates
   # POST /donates.json
   def create
@@ -36,30 +32,6 @@ class DonatesController < ApplicationController
         format.html { render :new }
         format.json { render json: @donate.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /donates/1
-  # PATCH/PUT /donates/1.json
-  def update
-    respond_to do |format|
-      if @donate.update(donate_params)
-        format.html { redirect_to @donate, notice: 'Donate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @donate }
-      else
-        format.html { render :edit }
-        format.json { render json: @donate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /donates/1
-  # DELETE /donates/1.json
-  def destroy
-    @donate.destroy
-    respond_to do |format|
-      format.html { redirect_to donates_url, notice: 'Donate was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
