ffba2772a0b7ac12e8a4422158fc3e8fc7592e1c
Jônatas Rodrigues <jonatas.rodriguess@gmail.com>
2019-06-12 22:27:56 -0300

    Criando página de projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  get '/project/:id', to: 'pages#show_project'
   devise_for :users
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
