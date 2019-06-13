a8b67d7bb37ec07d4fce9fe76cedc91c787896b9
Jonatas Rodrigues Alves <jonatasalves@ntbloc82681.local>
2019-06-13 14:34:41 -0300

    Ajuste de modelo de projetos

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,10 +5,6 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :amount, presence: true, numericality: true
   validates :image, presence: true
-  validates :donate, presence: true
 
   mount_uploader :image, ImageUploader
-
-  def donate
-  end
 end
